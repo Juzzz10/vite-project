97260f5be4f21b74a15a3da791ab68aad13d7083
Juzzz10 <justin9953enriquez@gmail.com>
2025-02-06 14:40:00 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Justine Jay A. Enriquez</h1>
+      <div id="rectangle">CPEITEL</div>
+    </div>
+  )
+}
+
+export default App
